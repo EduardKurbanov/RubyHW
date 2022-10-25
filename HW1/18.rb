209318b74b5760adab2b830508f4157af4f48343
Edward <edward89@ukr.net>
2022-10-25 16:47:27 +0300

    fix.....

--- HW1/18.rb
+++ HW1/18.rb
@@ -3,3 +3,5 @@
 p 'Дан целочисленный массив. Вывести вначале все его четные элементы, а затем - нечетные.'
 p '(1..10).to_a.select{ |i| i.even? } + (1..10).to_a.select{ |i| i.odd? }'
 p (1..10).to_a.select(&:even?) + (1..10).to_a.select(&:odd?)
+p '(1..10).to_a.partition { |i| i.even?}.flatten(1)'
+p (1..10).to_a.partition { |i| i.even?}.flatten(1)
--- HW1/19.rb
+++ HW1/19.rb
@@ -3,3 +3,5 @@
 p 'Дан целочисленный массив. Вывести вначале все его нечетные элементы, а затем - четные.'
 p '(1..10).to_a.select{ |i| i.odd? } + (1..10).to_a.select{ |i| i.even? }'
 p (1..10).to_a.select(&:odd?) + (1..10).to_a.select(&:even?)
+p '(1..10).to_a.partition { |i| i.even?}reverse.flatten(1)'
+p (1..10).to_a.partition { |i| i.even?}.reverse.flatten(1)
