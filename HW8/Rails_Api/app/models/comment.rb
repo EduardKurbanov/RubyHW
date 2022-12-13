3fe5771702ee0756ab0cabb44036486eb37afc7e
EduardKurbanov <edward89@ukr.net>
2022-12-13 20:25:57 +0200

    fixHW8_v0.0.4

--- HW8/Rails_Api/app/models/comment.rb
+++ HW8/Rails_Api/app/models/comment.rb
@@ -7,7 +7,7 @@ class Comment < ApplicationRecord
 
   enum :status, { unpublished: 0, published: 1 }, default: :unpublished
 
-  scope :status_position_comment, ->(status_ps) { where(status: status_ps) }
+  scope :status_position_comment, ->(status) { where(status: status) }
   scope :get_all_by_sort_desc , -> { order('created_at DESC') }
   scope :get_comment_limit, ->(limit_com = 10) { order('created_at DESC').limit(limit_com) }
 end
