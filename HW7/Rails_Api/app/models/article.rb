ee15caf2b5fd1da0772fa82e38742e95b6a9c57c
EduardKurbanov <edward89@ukr.net>
2022-12-15 11:49:15 +0200

    fix_tag_hw7

--- HW7/Rails_Api/app/models/article.rb
+++ HW7/Rails_Api/app/models/article.rb
@@ -11,4 +11,14 @@ class Article < ApplicationRecord
   enum :status, { unpublished: 0, published: 1 }, default: :unpublished
 
   scope :get_articles_all, -> { order('created_at DESC') }
+
+  def all_tags=(names)
+    self.tags = names.split(",").map do |name|
+        Tag.where(title: name.strip).first_or_create!
+    end
+  end
+  
+  def all_tags
+    self.tags.map(&:title).join(", ")
+  end
 end
