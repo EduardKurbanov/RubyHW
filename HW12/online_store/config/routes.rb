4c028c87cba980e5efc4d8f3dbd7df81a371e2fe
EduardKurbanov <edward89@ukr.net>
2023-01-12 22:36:54 +0200

    hw12_mod_routes

--- HW12/online_store/config/routes.rb
+++ HW12/online_store/config/routes.rb
@@ -12,10 +12,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :products
-  resources :categories
-  #resources :line_items, onli: %i[create]
-  resources :orders, onli: %i[create show index new]
+  resources :products, only: %i[show index]
+  resources :categories, only: %i[show index]
+  resources :line_items
+  resources :orders, only: %i[create show index new]
   
   get "/about", to: redirect("/about.html")
   get "/faqs", to: redirect("/faqs.html")
@@ -24,10 +24,10 @@ Rails.application.routes.draw do
 
   get "/cart", to: "carts#show", as: "cart"
   patch 'orders/:id/switch', to: 'orders#switch_status', as: 'switch'
-  post 'line_items' => 'line_items#create'
-  get 'line_items/:id' => "line_items#destroy", as: "line_item"
-  get 'line_items/:id/add' => 'line_items#add_quantity', as: 'line_item_add'
-  get 'line_items/:id/reduce' => 'line_items#reduce_quantity', as: 'line_item_reduce'
+#  post 'line_items' => 'line_items#create'
+#  get 'line_items/:id' => "line_items#destroy", as: "line_item"
+#  get 'line_items/:id/add' => 'line_items#add_quantity', as: 'line_item_add'
+#  get 'line_items/:id/reduce' => 'line_items#reduce_quantity', as: 'line_item_reduce'
 
 
   match '*unmatched', to: 'application#notfound!', via: :all
