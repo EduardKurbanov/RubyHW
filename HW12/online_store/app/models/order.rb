f84e0e9f8e48ed508bfcadcc3574173fe8ea8db0
EduardKurbanov <edward89@ukr.net>
2023-01-12 20:32:08 +0200

    gem install annotate

--- HW12/online_store/app/models/order.rb
+++ HW12/online_store/app/models/order.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: orders
+#
+#  id         :bigint           not null, primary key
+#  cart_id    :bigint           not null
+#  user_id    :bigint           not null
+#  status     :integer          default("unpaid")
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  name       :string
+#  email      :string
+#  address    :text
+#
 class Order < ApplicationRecord
   belongs_to :cart
   belongs_to :user
