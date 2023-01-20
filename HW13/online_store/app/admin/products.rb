3738cddf3dd0a3eaad824a272659233ac97f79dc
EduardKurbanov <edward89@ukr.net>
2023-01-20 18:12:12 +0200

    hw13_add_form_products

--- HW13/online_store/app/admin/products.rb
+++ HW13/online_store/app/admin/products.rb
@@ -6,6 +6,19 @@ ActiveAdmin.register Product do
   # Uncomment all parameters which should be permitted for assignment
   #
   permit_params :name, :description, :price, :image, :category_id
+
+  includes :category
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :description
+      f.input :price
+      f.input :image, as: :file
+      f.input :category_id
+    end
+    f.actions
+  end
   #
   # or
   #
