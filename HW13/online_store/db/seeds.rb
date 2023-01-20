e7d7d752c18c49b73562eb36332445077d6a8086
EduardKurbanov <edward89@ukr.net>
2023-01-20 18:26:45 +0200

    removed product due to has_one_attached :image, dependent: :destroy, didn't want to check db:seed

--- HW13/online_store/db/seeds.rb
+++ HW13/online_store/db/seeds.rb
@@ -7,6 +7,8 @@
 #   Character.create(name: "Luke", movie: movies.first)
 Category.destroy_all
 Product.destroy_all
+User.destroy_all
+AdminUser.destroy_all
 
 Category.create([
     {
@@ -21,50 +23,7 @@ Category.create([
 
 ])
 
-Product.create([
-    {
-        name: "amd phenom ii x 6",
-        description: "amd phenom ii x 6 T1090",
-        price: 1000,
-        image: "https://static6.depositphotos.com/1014550/624/i/600/depositphotos_6240474-stock-photo-test-word-on-keyboard.jpg",
-        category_id: 1
-    },
-    {
-        name: "intel xeon",
-        description: "intel xeon intel xeon intel xeon",
-        price: 2000,
-        image: "https://static6.depositphotos.com/1014550/624/i/600/depositphotos_6240474-stock-photo-test-word-on-keyboard.jpg",
-        category_id: 1
-    },
-    {
-        name: "H.A.W.X",
-        description: "AIR WAR",
-        price: 100,
-        image: "https://static6.depositphotos.com/1014550/624/i/600/depositphotos_6240474-stock-photo-test-word-on-keyboard.jpg",
-        category_id: 2
-    },
-    {
-        name: "gta vc",
-        description: "gta vc gta vc gta vc",
-        price: 20,
-        image: "https://static6.depositphotos.com/1014550/624/i/600/depositphotos_6240474-stock-photo-test-word-on-keyboard.jpg",
-        category_id: 2
-    },
-    {
-        name: "a4tech",
-        description: "a4tech a4tech a4tech",
-        price: 10,
-        image: "https://static6.depositphotos.com/1014550/624/i/600/depositphotos_6240474-stock-photo-test-word-on-keyboard.jpg",
-        category_id: 3
-    },
-    {
-        name: "logitech",
-        description: "car car car",
-        price: 50,
-        image: "https://static6.depositphotos.com/1014550/624/i/600/depositphotos_6240474-stock-photo-test-word-on-keyboard.jpg",
-        category_id: 3
-    }
-])
+
 
 User.create!(email: 'user@ukr.com', password: 'password', password_confirmation: 'password')
 AdminUser.create!(email: 'admin@ukr.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
