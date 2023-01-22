9959137f7d67f54182522fe33581b37fad02b547
EduardKurbanov <edward89@ukr.net>
2023-01-22 20:24:16 +0200

    mod_mailer

--- HW13/online_store/app/mailers/user_mailer.rb
+++ HW13/online_store/app/mailers/user_mailer.rb
@@ -1,2 +1,13 @@
 class UserMailer < ApplicationMailer
+
+  def order(user, order)
+    @order = order
+    @line_items = order.cart.line_items.includes(:product)
+    mail(to: user.email, subject: 'Your order')
+  end
+
+  def welcome_message(user)
+    @user = user
+    mail(to: @user.email, subject: 'New account information')
+  end
 end
