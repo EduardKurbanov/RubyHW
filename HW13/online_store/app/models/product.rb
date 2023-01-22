f4262c34cc2f752d62390ecb2d788bf2e8f7277a
EduardKurbanov <edward89@ukr.net>
2023-01-22 12:39:10 +0200

    mod_models_product_image_attach

--- HW13/online_store/app/models/product.rb
+++ HW13/online_store/app/models/product.rb
@@ -15,7 +15,11 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :line_items, dependent: :nullify
   
-  has_one_attached :image, dependent: :destroy
+  has_one_attached :image do |attachable|
+    attachable.variant :little, resize_to_limit: [100, 100]
+    attachable.variant :medium, resize_to_limit: [200, 200]
+    attachable.variant :main, resize_to_limit: [235, 235]
+  end
 
   validates :name, :description, :price, presence: true
 end
