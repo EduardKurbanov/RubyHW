6bd0f161be27e9c3af5ee2387920bcfe1e2374cf
EduardKurbanov <edward89@ukr.net>
2023-01-22 12:28:53 +0200

    add_categories_include

--- HW13/online_store/app/controllers/categories_controller.rb
+++ HW13/online_store/app/controllers/categories_controller.rb
@@ -1,12 +1,11 @@
 class CategoriesController < ApplicationController
 
   def index
-    @categories = Category.all
+    @categories = Category.all.with_attached_image
   end
 
   def show
     @category = Category.find(params[:id])
-    @products = @category.products
-  end
+    @products = @category.products.with_attached_image
 end
 
